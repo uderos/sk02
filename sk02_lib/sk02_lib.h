4467fc8f22d4fd26e2835abbc5a32553810abb44
uderos <uderos@ilww.com>
2018-07-10 11:31:34 -0400

    wip

---
+++ sk02_lib/sk02_lib.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "Cell.h"
--- utest01/test.cpp
+++ utest01/test.cpp
@@ -1,10 +1,31 @@
 #include "pch.h"
-#include "Cell.h"
+#include "sk02_lib.h"
 
-TEST(TestCaseName, TestName)
+TEST(CellClassTest, Constructor)
 {
 	Cell c;
 	EXPECT_FALSE(c.is_solved());
 	EXPECT_TRUE(c.has_candidate(0));
 	EXPECT_TRUE(c.has_candidate(9));
+}
+
+TEST(CellClassTest, Set)
+{
+	Cell c;
+	EXPECT_FALSE(c.is_solved());
+	EXPECT_TRUE(c.has_candidate(0));
+	c.set(4);
+	EXPECT_TRUE(c.is_solved());
+	EXPECT_FALSE(c.has_candidate(0));
+}
+
+TEST(CellClassTest, ClearCandidates)
+{
+	Cell c;
+
+	EXPECT_FALSE(c.is_solved());
+	EXPECT_TRUE(c.has_candidate(0));
+	c.set(4);
+	EXPECT_TRUE(c.is_solved());
+	EXPECT_FALSE(c.has_candidate(0));
 }
