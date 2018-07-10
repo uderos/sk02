4d6c0eb90dd3acda2e5f796842b6a8620986f693
uderos <uderos@ilww.com>
2018-07-10 11:37:08 -0400

    wip

--- utest01/test.cpp
+++ utest01/test.cpp
@@ -23,9 +23,31 @@ TEST(CellClassTest, ClearCandidates)
 {
 	Cell c;
 
-	EXPECT_FALSE(c.is_solved());
-	EXPECT_TRUE(c.has_candidate(0));
-	c.set(4);
+	for (int i = 0; i < 10; ++i)
+		EXPECT_TRUE(c.has_candidate(i));
+
+	for (int i = 0; i < 8; ++i)
+	{
+		c.clear_candidate(i);
+		EXPECT_FALSE(c.is_solved());
+		EXPECT_FALSE(c.has_candidate(i));
+		EXPECT_TRUE(c.has_candidate(i + 1));
+	}
+
+	c.clear_candidate(8);
 	EXPECT_TRUE(c.is_solved());
-	EXPECT_FALSE(c.has_candidate(0));
+	EXPECT_EQ(c.get(), 9);
+}
+
+TEST(CellClassTest, ErrorChecks01)
+{
+	Cell c;
+
+	EXPECT_THROW(c.get(), std::runtime_error);
+
+	EXPECT_THROW(c.has_candidate(-1), std::runtime_error);
+	EXPECT_THROW(c.has_candidate(10), std::runtime_error);
+
+	EXPECT_THROW(c.set(-1), std::runtime_error);
+	EXPECT_THROW(c.set(10), std::runtime_error);
 }
