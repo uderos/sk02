5c727d479832dd2aac14307539bddec1e77b40b2
uderos <uderos@ilww.com>
2018-07-11 09:58:15 -0400

    wip

--- utest01/test02.cpp
+++ utest01/test02.cpp
@@ -10,11 +10,6 @@ TEST(BoardTest, Constructor)
 TEST(BoardTest, TestSetGeneration)
 {
 	Board b;
-	Cell * cell_ptr_array[BOARD_SIZE][BOARD_SIZE];
-
-	for (int rx = 0; rx < BOARD_SIZE; ++rx)
-		for (int cx = 0; cx < BOARD_SIZE; ++cx)
-			cell_ptr_array[rx][cx] = (&b(rx, cx));
 
 	// test rows
 	int idx = 0;
@@ -24,7 +19,7 @@ TEST(BoardTest, TestSetGeneration)
 		for (int cx = 0; cx < BOARD_SIZE; ++cx)
 		{
 			Cell * cell_ptr = (&row.get_cell(cx));
-			EXPECT_EQ(cell_ptr, cell_ptr_array[rx][cx]);
+			EXPECT_EQ(cell_ptr, &b(rx, cx));
 		}
 	}
 
@@ -35,7 +30,7 @@ TEST(BoardTest, TestSetGeneration)
 		for (int rx = 0; rx < BOARD_SIZE; ++rx)
 		{
 			Cell * cell_ptr = (&column.get_cell(rx));
-			EXPECT_EQ(cell_ptr, cell_ptr_array[rx][cx]);
+			EXPECT_EQ(cell_ptr, &b(rx,cx));
 		}
 	}
 
@@ -45,20 +40,37 @@ TEST(BoardTest, TestSetGeneration)
 	{
 		CellRefSet & group = b.get_set(idx++);
 
-		int i = 0;
+		int cell_idx = 0;
 		for (int rj = 0; rj < step; ++rj)
 		{
 			for (int cj = 0; cj < step; ++cj)
 			{
-				Cell * group_cell_ptr = (&group.get_cell(i++));
+				Cell * group_cell_ptr = (&group.get_cell(cell_idx++));
 
 				const int rx = (3*(gx / 3)) + rj;
 				const int cx =  (3*(gx % 3)) + cj;
 				// std::cout << "gx=" << gx << " rx=" << rx << " cx=" << cx << std::endl;
 
-				Cell * board_cell_ptr = cell_ptr_array[rx][cx];
+				Cell * board_cell_ptr = &b(rx,cx);
 				EXPECT_EQ(group_cell_ptr, board_cell_ptr);
 			}
 		}
 	}
+}
+
+TEST(BoardTest, IsSolved)
+{
+	Board b;
+	EXPECT_FALSE(b.is_solved());
+
+	for (int rx = 0; rx < BOARD_SIZE; ++rx)
+	{
+		for (int cx = 0; cx < BOARD_SIZE; ++cx)
+		{
+			EXPECT_FALSE(b.is_solved());
+			b(rx, cx).set(1);
+		}
+	}
+
+	EXPECT_TRUE(b.is_solved());
 }
